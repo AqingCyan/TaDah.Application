19b1e2b3e4bc7411a303208467feaf3980dc7c4e
AqingCyan <aqingcyan@gmail.com>
2022-12-02 00:43:19 +0800

    fix: login logic error

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -4,24 +4,21 @@ import { API } from '../../../typings'
 /**
  * 获取登录用户信息
  */
-export const pingCurrentUser = (): API.FetchResponse<USER.UserInfo> =>
-  request.get('/auth/ping', {
-    headers: { Authorization: `Bearer ${window.localStorage.getItem('bearer_token')}` },
-  })
+export const pingCurrentUser = (): API.FetchResponse<USER.UserInfo> => request().get('/auth/ping')
 
 /**
  * 检查用户是否注册过
  * @param uid
  */
 export const checkRegistration = (uid: string): API.FetchResponse<boolean> =>
-  request.get('/account/checkRegistration', { params: { uid } })
+  request().get('/account/checkRegistration', { params: { uid } })
 
 /**
  * 上传头像
  * @param formData
  */
 export const uploadHeadImage = (formData: FormData): API.FetchResponse<string> =>
-  request.post('/cos', {
+  request().post('/cos', {
     data: formData,
   })
 
@@ -38,11 +35,11 @@ export const registerAccount = (
     email: string
     password: string
   },
-): API.FetchResponse<boolean> => request.post('/account/registerPassword', { params: { uid }, data })
+): API.FetchResponse<boolean> => request().post('/account/registerPassword', { params: { uid }, data })
 
 /**
  * 登录
  * @param data
  */
 export const loginAccount = (data: { uid: string; password: string }): API.FetchResponse<string> =>
-  request.post('/auth/login', { data })
+  request().post('/auth/login', { data })
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -8,17 +8,18 @@ const baseURL = {
   prod: 'https://www.cyanthing.com',
 }[env]
 
-const request = extend({
-  prefix: baseURL,
-  headers: { Authorization: `Bearer ${window.localStorage.getItem('bearer_token')}` },
-  errorHandler: (err: { data: { statusCode: number; message: string } }) => {
-    const { data } = err
-    const { location } = window
-    if (data.statusCode === 401) {
-      history.push(`/login${location.search}`)
-    }
-    return data
-  },
-})
+const request = () =>
+  extend({
+    prefix: baseURL,
+    headers: { Authorization: `Bearer ${window.localStorage.getItem('bearer_token')}` },
+    errorHandler: (err: { data: { statusCode: number; message: string } }) => {
+      const { data } = err
+      const { location } = window
+      if (data.statusCode === 401) {
+        history.push(`/login${location.search}`)
+      }
+      return data
+    },
+  })
 
 export default request
